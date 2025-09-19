f4fa3585bd8c37540251f11b68ba72b557366fae
adlaidlaw <advlaidlaw@gmail.com>
2025-09-18 22:29:43 -0400

    Update toc.js
    added in the missing comma syntax after titles

--- toc.js
+++ toc.js
@@ -2,16 +2,16 @@
 const toc = [
   { title: "PI Chapter 1", url: "PI-Chapter 1.html" },
   { title: "PI Chapter 2", url: "PI-Chapter 2.html" },
-  { title: "PI Chapter 3", url: "PI-Chapter 3.html" }
-  { title: "PI Chapter 4", url: "PI-Chapter 4.html" }
-  { title: "PII Chapter 1", url: "PII-Chapter 1.html" }
-  { title: "PII Chapter 2", url: "PII-Chapter 2.html" }
-  { title: "PII Chapter 3", url: "PII-Chapter 3.html" }
-  { title: "PII Chapter 4", url: "PII-Chapter 4.html" }
-  { title: "PII Chapter 5", url: "PII-Chapter 5.html" }
-  { title: "PII Chapter 6", url: "PII-Chapter 6.html" }
-  { title: "PIII Chapter 1", url: "PIII-Chapter 2.html" }
-  { title: "PIII Chapter 2", url: "PIII-Chapter 2.html" }
+  { title: "PI Chapter 3", url: "PI-Chapter 3.html" },
+  { title: "PI Chapter 4", url: "PI-Chapter 4.html" },
+  { title: "PII Chapter 1", url: "PII-Chapter 1.html" },
+  { title: "PII Chapter 2", url: "PII-Chapter 2.html" },
+  { title: "PII Chapter 3", url: "PII-Chapter 3.html" },
+  { title: "PII Chapter 4", url: "PII-Chapter 4.html" },
+  { title: "PII Chapter 5", url: "PII-Chapter 5.html" },
+  { title: "PII Chapter 6", url: "PII-Chapter 6.html" },
+  { title: "PIII Chapter 1", url: "PIII-Chapter 2.html" },
+  { title: "PIII Chapter 2", url: "PIII-Chapter 2.html" },
 ];
 
 function renderTOC() {
